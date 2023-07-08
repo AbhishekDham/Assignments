a3939dc9d608e24c3fecfd3ece243b553266f6ad
Abhishek <Abhishek@DESKTOP-JQK94OT>
2023-07-09 00:31:48 +0530

    Commit

---
+++ src/Assignments/Intreturn.java
@@ -0,0 +1,31 @@
+package Assignments;
+
+public class Intreturn {
+	
+public static void main(String[] args) {
+		
+	}
+	
+}
+
+class Test{
+	
+	// Static method with int return
+	
+	static int sum(int i,int j){
+		int k =i+j;
+		return k;
+		
+	}
+	
+	// Static method with int return
+	
+		int multiply(int i,int j){
+			int k =i*j;
+			return k;
+	
+		}
+	
+	
+}
+
