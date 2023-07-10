220b530e949f3911b8d9ef9336fc4bb845d2258b
Abhishek <Abhishek@DESKTOP-JQK94OT>
2023-07-11 01:23:53 +0530

    Commit

---
+++ src/Assignments/Tempscale.java
@@ -0,0 +1,19 @@
+package Assignments;
+
+public class Tempscale {
+
+	public static void main(String[] args) {
+		double   c = 0.0;
+		double f = 1.8*c + 32;
+		double k = c + 273;
+		System.out.println(c + " Celsius " + " = " + f + " Fahrenheit ");
+		System.out.println(c + " Celsius " + " = " + k + " Kelvin ");
+		
+		
+		
+	}
+	
+	
+	
+	
+}
