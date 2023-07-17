627c2f4b42670b2f7a7578e02dee66bb02fc2b54
Abhishek <Abhishek@DESKTOP-JQK94OT>
2023-07-18 00:32:27 +0530

    Commit

---
+++ src/Assignments/Staticblock.java
@@ -0,0 +1,34 @@
+package Assignments;
+
+public class Staticblock {
+ 
+	public static void main(String[] args) {
+		
+	}
+}
+
+class P{
+
+
+
+static{
+
+System.out.println("first block");  
+
+  }
+
+  static{
+
+System.out.println("second block");  
+
+  }
+
+public static void main(String[] args){
+
+System.out.println("main method");
+
+}
+
+
+
+  }
