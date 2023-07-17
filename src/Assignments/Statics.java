194366e4fb09dcd71c80544770ee626432807a7a
Abhishek <Abhishek@DESKTOP-JQK94OT>
2023-07-18 00:25:12 +0530

    Comit

---
+++ src/Assignments/Statics.java
@@ -0,0 +1,33 @@
+package Assignments;
+
+public class Statics {
+
+public static void main(String[] args) {
+	
+}
+
+}
+
+	class A1{
+
+		static int i = 10;
+
+		}
+
+		class B1{
+
+		public static void main(String[] args){
+
+		System.out.println(A1.i);
+
+		A1 a1 = new A1();
+
+		System.out.println(a1.i);
+
+		}
+
+		}
+
+	
+	
+
