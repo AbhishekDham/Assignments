3560acc5b716d056deff8952869ebf3cee000965
Abhishek <Abhishek@DESKTOP-JQK94OT>
2023-08-11 01:12:02 +0530

    Commit

---
+++ src/Assignments/Uppertolower.java
@@ -0,0 +1,33 @@
+package Assignments;
+
+public class Uppertolower {
+
+	public static void main(String[] args) {
+		
+		String str = "AbC#$dEf";
+		StringBuffer newStr=new StringBuffer(str);
+		
+		for(int i =0; i<str.length();i++) {
+			
+			if(Character.isLowerCase(str.charAt(i))) {
+			newStr.setCharAt(i, Character.toUpperCase(str.charAt(i)));
+				
+				
+				
+			}
+			
+			else if(Character.isUpperCase(str.charAt(i))) {
+				newStr.setCharAt(i, Character.toLowerCase(str.charAt(i)));
+				
+				
+			}
+			
+		}
+		
+		System.out.println(newStr);
+		
+		
+		
+		
+	}
+}
